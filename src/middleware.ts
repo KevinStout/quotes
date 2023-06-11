b7d0d697571a1cd1c6db17eafc59b6596f1c49e6
Kevin Stout <stoutkev3@gmail.com>
2023-06-11 14:02:50 -0400

    adding new post route and removing example route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,22 @@
-// import { authMiddleware } from "@clerk/nextjs";
+// This file contains the middle ware for clerk both the old way and new way 
+import { authMiddleware } from "@clerk/nextjs";
 
-// export default authMiddleware();
+export default authMiddleware();
 
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export default withClerkMiddleware((req: NextRequest) => {
-  console.log("MIDDLEWARE CALLED");
-  return NextResponse.next();
-});
-
-// Stop Middleware running on static files
 export const config = {
-  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+// import { withClerkMiddleware } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+
+// export default withClerkMiddleware((req: NextRequest) => {
+//   console.log("MIDDLEWARE CALLED");
+//   return NextResponse.next();
+// });
+
+// // Stop Middleware running on static files
+// export const config = {
+//   matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+// };
