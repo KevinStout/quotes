30dbb8de07147db5db707cc6791d0831de0bf75c
Kevin Stout <stoutkev3@gmail.com>
2023-06-18 14:41:10 -0400

    updating middleware to allow posts to be read before the need of sign in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 // This file contains the middle ware for clerk both the old way and new way
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/", "/api/trpc/posts.getAll"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
